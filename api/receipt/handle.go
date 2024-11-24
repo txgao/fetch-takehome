ca2538cd246dbc083ab4ddf97b764af9c701b117
txgao <txgao71@gmail.com>
2024-11-24 10:22:04 -0800

    update error response

--- api/receipt/handle.go
+++ api/receipt/handle.go
@@ -2,6 +2,7 @@ package receipt
 
 import (
 	"errors"
+	errorcode "fetch-takehome/pkg/errors"
 	"fetch-takehome/pkg/receipt"
 	"log/slog"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"time"
 
 	"github.com/go-chi/render"
+	"github.com/google/uuid"
 )
 
 type Handle struct {
@@ -18,15 +20,39 @@ type Handle struct {
 
 func ErrorResponse(statusCode int, message string) *Response {
 	return &Response{
-		Code:        statusCode,
-		body:        message,
+		Code: statusCode,
+		body: map[string]interface{}{
+			"code":    statusCode,
+			"message": message,
+		},
 		contentType: "application/json",
 	}
 
 }
 
 func (h Handle) GetIDPoints(w http.ResponseWriter, r *http.Request, id string) *Response {
-	return ErrorResponse(http.StatusNotImplemented, "GetReceiptPoint not implemented")
+
+	receipt_uuid, err := uuid.Parse(id)
+	if err != nil {
+		slog.Error("Error parsing receipt uuid", "err", err, "id", id)
+		return ErrorResponse(http.StatusBadRequest, "unable to parse receipt id")
+	}
+
+	points, err := h.ReceiptService.GetReceiptPoint(r.Context(), receipt_uuid)
+	if err != nil {
+		slog.Error("Error finding receipt by uuid", "err", err, "id", receipt_uuid)
+		if errors.Is(err, errorcode.ErrReceiptNotFound) {
+			return ErrorResponse(http.StatusNotFound, "receipt not found")
+		}
+		return ErrorResponse(http.StatusInternalServerError, "unable to get receipt")
+	}
+
+	body := struct {
+		Points *int64 `json:"points,omitempty"`
+	}{
+		Points: &points,
+	}
+	return GetIDPointsJSON200Response(body)
 }
 
 func (h Handle) PostProcess(w http.ResponseWriter, r *http.Request) *Response {
@@ -38,7 +64,7 @@ func (h Handle) PostProcess(w http.ResponseWriter, r *http.Request) *Response {
 	}
 
 	// Parse the date time into time.Time
-	purchaseDateTime := data.PurchaseDate.String() + " " + data.PurchaseTime
+	purchaseDateTime := strings.TrimSpace(data.PurchaseDate.String()) + " " + strings.TrimSpace(data.PurchaseTime)
 	layout := "2006-01-02 15:04"
 	timeStamp, err := time.Parse(layout, purchaseDateTime)
 	if err != nil {
@@ -63,7 +89,7 @@ func (h Handle) PostProcess(w http.ResponseWriter, r *http.Request) *Response {
 		}
 		temp := receipt.Item{
 			Price:            floatPrice,
-			ShortDescription: item.ShortDescription,
+			ShortDescription: strings.TrimSpace(item.ShortDescription),
 		}
 		items = append(items, temp)
 	}
@@ -89,6 +115,7 @@ func (h Handle) PostProcess(w http.ResponseWriter, r *http.Request) *Response {
 	return PostProcessJSON200Response(body)
 }
 
+// ensure value is not less than 0
 func verifyFloatValue(s string) (float64, error) {
 	float, err := strconv.ParseFloat(s, 64)
 	if err != nil {
