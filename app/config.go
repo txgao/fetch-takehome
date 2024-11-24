8c237f6bfd000c1cc5c1e0b1d32a47b46ff3c75f
txgao <txgao71@gmail.com>
2024-11-24 10:45:12 -0800

    update config default value

--- app/config.go
+++ app/config.go
@@ -1,7 +1,7 @@
 package app
 
 type Server struct {
-	Host string `env:"HOST" env-default:"localhost"`
+	Host string `env:"HOST" env-default:"0.0.0.0"`
 	Port int    `env:"PORT" env-default:"4000"`
 }
 
